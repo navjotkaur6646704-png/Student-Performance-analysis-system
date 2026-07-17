6d7954019784a7191b196ca6b21b1f5ff50578de
Navjot Kaur <navjotkaur6646704@gmail.com>
2026-07-17 16:42:42 +0430

    Fix typo in dashboard startup message

--- dashboard.py
+++ dashboard.py
@@ -1,8 +1,9 @@
 import streamlit as st
 import pandas as pd
 import matplotlib.pyplot as plt
+import prediction
 
-st.write("Dashboard Starte")
+st.write("Dashboard Started")
 
 #page configuration 
 
